35b6f1bf0b53764f49f327199384dca11053bdbc
Sakari Mursu <sakari@mursu.dev>
2020-09-03 19:27:20 +0300

    Init ts-kit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ["js", "json", "ts", "node"],
+  testEnvironment: "node",
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/"],
+};
